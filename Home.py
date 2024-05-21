50c78fafc0bdd68a53538ae6f1457c340542d96e
DimitriKneur <dimitri.kneur@gmail.com>
2024-05-21 02:25:23 +0200

    modify home

--- Home.py
+++ Home.py
@@ -3,7 +3,26 @@ import streamlit as st
 st.title("Le Louvre Works of Art on Display - Databases")
 
 st.text("Welcome, dear explorer!")
-st.text("The objective of this application is to show you an interactive table of the works of art that are currently on display at one of the most prestigious museums worldwide: the famous Le Louvre museum in Paris, France!")
+st.text("The objective of this application is to show you an interactive table of the works")
+st.text("of art that are currently on display at one of the most prestigious museums")
+st.text("worldwide: the famous Le Louvre museum in Paris, France!")
+
+st.text("For performance optimization reasons, the works of art have been separated")
+st.text("into two distinct parts :")
+
+st.markdown("-  **A) Antiquities part**: here you will find [](https://dimitri-kneur-le-louvre-works-of-art-on-display.streamlit.app/Antiquities)")
+st.markdown("  - Greek, Etruscan and Roman Antiquities")
+st.markdown("  - Oriental Antiquities")
+st.markdown("  - Egyptian Antiquities")
+
+st.markdown("-  **B) Other collections** : here you will find [](https://dimitri-kneur-le-louvre-works-of-art-on-display.streamlit.app/Other_Collections)")
+st.markdown("  - Paintings")
+st.markdown("  - Medieval, Renaissance and Modern Works of Art")
+st.markdown("  - Medieval, Renaissance and Modern Sculpture")
+st.markdown("  - Islamic Arts")
+st.markdown("  - Louvre History Department")
+st.markdown("  - Byzantine and Oriental Christian Arts")
+st.markdown("  - Eugène-Delacroix National Museum")
 
 st.text("I wish you a wonderful journey through this treasure trove of artistic masterpieces.")
 st.text("Regards,")
