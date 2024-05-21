7418f7455f77b0064638db1782efa4bb13a48402
DimitriKneur <dimitri.kneur@gmail.com>
2024-05-21 05:44:07 +0200

    antiquities

--- pages/Antiquities.py
+++ pages/Antiquities.py
@@ -3,21 +3,18 @@ import pandas as pd
 
 st.title("Antiquities on Display Database - Le Louvre Museum")
 
-st.text("You will find in this page the table showing the artworks of the following categories :")
+st.text("")
+
+st.text("You will find here the table showing the artworks of the following categories :")
 
 st.markdown("- Greek, Etruscan and Roman Antiquities")
 st.markdown("- Oriental Antiquities")
 st.markdown("- Egyptian Antiquities")
 
-st.text("Short of inspiration ? Here are some of the most famous artworks of these categories :")
-st.markdown("- Victoire de Samothrace")
-st.markdown("- Vénus de Milo")
-st.markdown("- Sarcophage des époux")
-st.markdown("- Le scribe accroupi")
-st.markdown("- Sphinx de Tanis")
+st.text("")
 
-st.text("If you want to display in full width, click on the top-right corner")
-st.text("'Fullscreen' button of the table :")
+st.text("If you want to display in full width, click on the top-right corner 'Fullscreen'⬇️")
+st.text("button of the table :")
 
 # Charger les données
 df = pd.read_csv('data/simplified_le_louvre_works_of_art_on_display_antiquities.csv')
@@ -36,3 +33,12 @@ st.data_editor(
     },
     hide_index=True,
 )
+
+st.text("Short of inspiration ? Some of the most famous artworks of these categories are :")
+st.markdown("- Victoire de Samothrace")
+st.markdown("- Vénus de Milo")
+st.markdown("- Sarcophage des époux")
+st.markdown("- Le scribe accroupi")
+st.markdown("- Sphinx de Tanis")
+
+st.text("")
