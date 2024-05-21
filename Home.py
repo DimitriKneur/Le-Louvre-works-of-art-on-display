f68888d50c91ad7f3a186007f42222d261874817
DimitriKneur <dimitri.kneur@gmail.com>
2024-05-21 03:14:46 +0200

    home

--- Home.py
+++ Home.py
@@ -1,10 +1,4 @@
 import streamlit as st
-from streamlit_extras.switch_page_button import switch_page
-
-def example():
-    want_to_contribute = st.button("I want to contribute!")
-    if want_to_contribute:
-        switch_page("Antiquities")
 
 st.title("Le Louvre Works of Art on Display - Databases")
 
