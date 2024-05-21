3ac8fc8f863292954148da518e86e939db828fd5
DimitriKneur <dimitri.kneur@gmail.com>
2024-05-21 04:10:01 +0200

    antiquities

--- pages/Antiquities.py
+++ pages/Antiquities.py
@@ -1,10 +1,48 @@
 import streamlit as st
 import pandas as pd
+from itables import show, init_notebook_mode
+from IPython.core.display import display, HTML
 
 st.title("Antiquities on Display Database - Le Louvre Museum")
 
 st.text("Welcome !")
 
+# Initialiser itables pour le mode interactif
+init_notebook_mode(all_interactive=True)
+
+# Charger les données
 df = pd.read_csv('data/simplified_le_louvre_works_of_art_on_display_antiquities.csv')
 
-st.dataframe(df, use_container_width=True)
+df["snapshot"] = [
+    '<a href="{url}">'
+    '<img src="{image}" width="50" '
+    'alt="Snapshot of {image}"></a>'.format(image=image, url=url)
+    for image, url in zip(df["image"], df["url"])
+]
+
+df["title"] = [
+    '<a href="{url}">{title}</a>'.format(title=title, url=url)
+    for title, url in zip(df["title"], df["url"])
+]
+
+# Déplacer la colonne 'snapshot' en première position
+col = df.pop('snapshot')
+df.insert(0, 'snapshot', col)
+
+# Supprimer les colonnes 'image' et 'url'
+df = df.drop(['image', 'room', 'url'], axis=1)
+
+# Afficher toutes les colonnes
+pd.set_option('display.max_columns', None)
+
+show(df,
+     maxBytes=0,
+     autoWidth=False,
+     scrollX=True,
+     columnDefs=[
+        {"targets": "_all", "className": "dt-head-center dt-body-left"},
+                ],
+     column_filters="header",
+     layout={"topEnd": None},
+     lengthMenu=[5, 10, 20, 50]
+     )
