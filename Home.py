34204e0d72f1e9c455ce44a19ce61c6385f7eff7
DimitriKneur <dimitri.kneur@gmail.com>
2024-05-21 02:28:16 +0200

    home

--- Home.py
+++ Home.py
@@ -10,12 +10,16 @@ st.text("worldwide: the famous Le Louvre museum in Paris, France!")
 st.text("For performance optimization reasons, the works of art have been separated")
 st.text("into two distinct parts :")
 
-st.markdown("-  **A) Antiquities part**: here you will find [](https://dimitri-kneur-le-louvre-works-of-art-on-display.streamlit.app/Antiquities)")
+st.text("")
+
+st.markdown("**A) Antiquities part**: here you will find [](https://dimitri-kneur-le-louvre-works-of-art-on-display.streamlit.app/Antiquities)")
 st.markdown("  - Greek, Etruscan and Roman Antiquities")
 st.markdown("  - Oriental Antiquities")
 st.markdown("  - Egyptian Antiquities")
 
-st.markdown("-  **B) Other collections** : here you will find [](https://dimitri-kneur-le-louvre-works-of-art-on-display.streamlit.app/Other_Collections)")
+st.text("")
+
+st.markdown("**[B) Other collections](https://dimitri-kneur-le-louvre-works-of-art-on-display.streamlit.app/Other_Collections)** : here you will find ")
 st.markdown("  - Paintings")
 st.markdown("  - Medieval, Renaissance and Modern Works of Art")
 st.markdown("  - Medieval, Renaissance and Modern Sculpture")
@@ -23,6 +27,8 @@ st.markdown("  - Islamic Arts")
 st.markdown("  - Louvre History Department")
 st.markdown("  - Byzantine and Oriental Christian Arts")
 st.markdown("  - Eugène-Delacroix National Museum")
+
+st.text("")
 
 st.text("I wish you a wonderful journey through this treasure trove of artistic masterpieces.")
 st.text("Regards,")
