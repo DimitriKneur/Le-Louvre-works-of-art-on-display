699b4d716e6292e6720f76946806b1b1d32c0be5
DimitriKneur <dimitri.kneur@gmail.com>
2024-05-21 05:39:49 +0200

    antiquities

--- pages/Antiquities.py
+++ pages/Antiquities.py
@@ -3,7 +3,21 @@ import pandas as pd
 
 st.title("Antiquities on Display Database - Le Louvre Museum")
 
-st.text("Welcome !")
+st.text("You will find in this page the table showing the artworks of the following categories :")
+
+st.markdown("- Greek, Etruscan and Roman Antiquities")
+st.markdown("- Oriental Antiquities")
+st.markdown("- Egyptian Antiquities")
+
+st.text("Short of inspiration ? Here are some of the most famous artworks of these categories :")
+st.markdown("- Victoire de Samothrace")
+st.markdown("- Vénus de Milo")
+st.markdown("- Sarcophage des époux")
+st.markdown("- Le scribe accroupi")
+st.markdown("- Sphinx de Tanis")
+
+st.text("If you want to display in full width, click on the top-right corner")
+st.text("'Fullscreen' button of the table :")
 
 # Charger les données
 df = pd.read_csv('data/simplified_le_louvre_works_of_art_on_display_antiquities.csv')
